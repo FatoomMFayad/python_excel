8c06de08a2a1f8aea7f36a5c103f1be73178698a
ffayad <ffayad@ptcdb.edu.ps>
2025-01-14 14:38:38 +0200

    create excel file and save list data to it

---
+++ save_excel.py
@@ -0,0 +1,31 @@
+import openpyxl as xl
+
+class Employee:
+    id = ''
+    name = ''
+    job_title = ''
+    salary = 0
+    def __init__(self, id, name, job_title, salary):
+        self.id = id
+        self.name = name
+        self.job_title = job_title
+        self.salary = salary
+
+e1 = Employee('123', 'Fatoom', 'CS Engineer', 20000)
+e2 = Employee('222', 'Hanan', 'Pharmacy Lead', 30000)
+e3 = Employee('233', 'Eman', 'Manager', 50000)
+e4 = Employee('444', 'Basma', 'Team Lead', 20000)
+employees = [e1, e2, e3, e4]
+
+wb = xl.Workbook()
+ws = wb.active
+ws['A1'] = 'ID'
+ws['B1'] = 'Name'
+ws['C1'] = 'Job Title'
+ws['D1'] = 'Salary'
+for employee in employees:
+    ws.append((employee.id, employee.name, employee.job_title, employee.salary))
+
+wb.save('C:/Users/Fatoom/Documents/employees.xlsx')
+
+
