63b04ed9b848adfb8e8e6b5edca059bb2e218f80
ffayad <ffayad@ptcdb.edu.ps>
2025-01-14 13:40:18 +0200

    reading excel file

---
+++ excel.py
@@ -0,0 +1,12 @@
+import openpyxl as xl
+
+file_name = 'C:/Users/Fatoom/Documents/countries.xlsx'
+try:
+    wb = xl.load_workbook(file_name)
+    sheet = wb.active
+
+    print(sheet.cell(row=1, column=1).value)
+    
+
+except FileNotFoundError as e:
+    print(e)
