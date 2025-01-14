45e31ba765f61a9363a6bd7646ed38608d6a8ed6
ffayad <ffayad@ptcdb.edu.ps>
2025-01-14 13:45:45 +0200

    iterate over rows

--- excel.py
+++ excel.py
@@ -5,8 +5,11 @@ try:
     wb = xl.load_workbook(file_name)
     sheet = wb.active
 
-    print(sheet.cell(row=1, column=1).value)
-    print(sheet['A2'].value)
+    # print(sheet.cell(row=1, column=1).value)
+    # print(sheet['A2'].value)
+
+    for row in sheet.iter_rows(values_only=True):
+        print(row)
 
 except FileNotFoundError as e:
     print(e)
